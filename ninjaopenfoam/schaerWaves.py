a3b3da7375fbb059584c8b64c8c93ca4a4d52b9e
James Shaw <js102@zepler.net>
2017-09-07 16:14:17 +0100

    create theta_diff and fast running of schaerWaves

--- ninjaopenfoam/schaerWaves.py
+++ ninjaopenfoam/schaerWaves.py
@@ -1,21 +1,25 @@
 from .case import Case
+from .errors import Errors
 from .dynamics import DynamicsExecution
 from .timing import Timing
 
 import os
 
 class SchaerWaves:
-    def __init__(self, name, mesh, timestep, fvSchemes, parallel, fast):
+    def __init__(self, name, mesh, timestep, fvSchemes, parallel, fast, fastMesh):
         self.case = Case(name)
 
         if fast:
             fvSchemes = os.path.join('src/schaerWaves/linearUpwind')
-            # TODO: fastMesh?
+            mesh = fastMesh
+            timestep = 120
+
+        self.timing = Timing(18000, 3600, timestep)
 
         self.dynamicsExecution = DynamicsExecution(
                 self.case,
                 mesh,
-                Timing(18000, 3600, timestep),
+                self.timing,
                 os.path.join('src/schaerWaves/Uf'),
                 os.path.join('src/schaerWaves/theta_init'),
                 os.path.join('src/schaerWaves/Exner_init'),
@@ -29,10 +33,15 @@ class SchaerWaves:
     def write(self, generator):
         g = generator
         case = self.case
+        endTime = str(self.timing.endTime)
 
         self.dynamicsExecution.write(generator)
+
+        errors = Errors(self.case, self.timing.endTime, 'theta')
+        errors.diff(generator)
+
+        generator.copy(self.case.path('0/theta'), self.case.path(endTime, 'theta_analytic'))
         
-        # TODO: theta_diff
         # TODO: s3upload
 
     def __str__(self):
