3ec894fe22a8e351fdd451b57acae85061ac3a3d
James Shaw <js102@zepler.net>
2017-09-07 16:08:29 +0100

    fixProcessorFaceVelocities if Uf exists

--- ninjaopenfoam/scripts/openfoam-solve.sh
+++ ninjaopenfoam/scripts/openfoam-solve.sh
@@ -16,6 +16,13 @@ taskCount=$2
 solver=$3
 
 decomposePar -force -time 0 -case $(realpath $case) # https://bugs.openfoam.org/view.php?id=2610
+
+if [ -e $case/0/Uf ]; then
+	for processorCase in $case/processor*; do
+		fixProcessorFaceVelocities -case $processorCase
+	done
+fi
+
 mpirun -np $taskCount $solver -parallel
 reconstructPar -case $case
 if [ -e $case/processor0/energy.dat ]; then
